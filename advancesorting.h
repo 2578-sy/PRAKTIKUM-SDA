93c25121b67788cccbfa55954755797c15f1de21
Syaula <syaulabalqiea@gmail.com>
2026-05-26 21:03:11 +0700

    Update advancesorting.h

--- advancesorting.h
+++ advancesorting.h
@@ -1,1 +1,13 @@
+#ifndef ADVANCE_SORTING_H
+#define ADVANCE_SORTING_H
 
+#define MAX_LENGTH 100
+
+void mergeSort(char arr[][MAX_LENGTH], int left, int right);
+void quickSort(char arr[][MAX_LENGTH], int low, int high);
+void shellSort(char arr[][MAX_LENGTH], int n);
+
+void merge(char arr[][MAX_LENGTH], int left, int mid, int right);
+int partition(char arr[][MAX_LENGTH], int low, int high);
+
+#endif
